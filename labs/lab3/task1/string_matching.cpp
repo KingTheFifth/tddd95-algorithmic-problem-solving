6c174e489323f87c7b1594b3753b982cf2d27fa3
Johannes Kung <johku144@student.liu.se>
2024-05-14 11:45:00 +0200

    Lab3: task 1, commented

--- labs/lab3/task1/string_matching.cpp
+++ labs/lab3/task1/string_matching.cpp
@@ -1,3 +1,9 @@
+/**
+ * Johannes Kung johku144
+ * Finding a pattern in a string using the Knuth-Morris-Pratt algorithm.
+ *
+ * Time complexity: O(text + pattern)
+ */
 #include <iostream>
 #include <ios>
 #include <string>
@@ -5,14 +11,29 @@
 
 using namespace std;
 
+/**
+ * Implementation of the string prefix function for Knuth-Morris-Pratt algorithm
+ * as done by CP-Algorithms (https://cp-algorithms.com/string/prefix-function.html).
+ *
+ * Time complexity: O(n) where n is the length of the given string s.
+ */
 vector<int> prefix_function(string s) {
   vector<int> pi(s.size());
+  // Calculate pi(i) for each i = 1,..., |s|-1
+  // (pi(0) = 0)
   for (int i = 1; i < s.size(); i++) {
+    // Save the length of the best prefix that is also a suffix of the substring 
+    // s[0...i-1]
     int j = pi[i-1];
+
+    // Reduce j to pi(j-1) until s[i] == s[j] or j == 0
     while (j > 0 && s[i] != s[j]) {
       j = pi[j-1];
     }
 
+    // If s[i] == s[j], then the prefix of length j+1 is a suffix of the 
+    // substring s[0...i] i.e. we have found the longest prefix
+    // Else j == 0 and there is no prefix of s[0...i] that is also a suffix
     if (s[i] == s[j]) {
       j++;
     }
@@ -23,7 +44,24 @@ vector<int> prefix_function(string s) {
   return pi;
 }
 
+/**
+ * Implementation of the Knuth-Morris-Pratt algorithm to find occurences of 
+ * a pattern in a string.
+ *
+ * Time complexity: O(pattern + phrase)
+ */
 vector<int> find(string pattern, string phrase) {
+  // Add the pattern onto the beginning of the phrase, with a delimiting character
+  // in between.
+  //
+  // The prefix function returns the length of the longest prefix of each substring
+  // phrase[0...i] such that the prefix is also a suffix of the substring. If 
+  // this length is the same as the length of the pattern, then this prefix 
+  // is exactly the pattern since it was appended to the beginning of the phrase.
+  //
+  // This tells us that the pattern can be found at position i - the lenght of 
+  // the pattern in the modified phrase, ending at position i.
+  //
   vector<int> positions;
   vector<int> pi = prefix_function(pattern + "#" + phrase);
   for (int i = 0; i < pi.size(); i++) {
